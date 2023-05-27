7a9e7fc98e00bd4ef1538b1d141604d3d650ee9c
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-05-27 19:20:54 +0300

    Assingment3Done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello");
+// Value to the grade variable
+let grade = 50;
+
+// Getting the grade message using the if statements
+
+if (grade >= 90 && grade <= 100) {
+  console.log("You got an A grade");
+} else if (grade >= 75 && grade < 90) {
+  console.log("You got a B grade");
+} else if (grade >= 60 && grade < 75) {
+  console.log("You got a C grade");
+} else {
+  console.log("You got an FAILED");
+}
